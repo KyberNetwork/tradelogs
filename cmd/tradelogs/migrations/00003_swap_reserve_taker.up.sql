2f6bf0454f059cf4c6c673c2e6435fa442d281e7
minhthanh.vu <ngocthanh1389@gmail.com>
2023-05-22 15:12:47 +0700

    update migration

--- cmd/tradelogs/migrations/00003_swap_reserve_taker.up.sql
+++ cmd/tradelogs/migrations/00003_swap_reserve_taker.up.sql
@@ -1,6 +1,6 @@
-UPDATE on_chain_trade SET 
-input_asset_symbol = output_asset_symbol,
-output_asset_symbol = input_asset_symbol,
-input_amount = output_amount,
-output_amount = input_amount
+UPDATE tradelogs SET 
+maker_token = taker_token,
+taker_token = maker_token,
+maker_token_amount = taker_token_amount,
+taker_token_amount = maker_token_amount
 WHERE taker = '0x807cf9a772d5a3f9cefbc1192e939d62f0d9bd38';
