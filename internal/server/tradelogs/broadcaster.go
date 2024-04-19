237323c53fe822ee0874534bebd186c953098505
minhthanh.vu <ngocthanh1389@gmail.com>
2024-04-19 11:08:04 +0700

    update mutex lock

--- internal/server/tradelogs/broadcaster.go
+++ internal/server/tradelogs/broadcaster.go
@@ -35,47 +35,21 @@ func NewBroadcaster(tradeChan chan storage.TradeLog) *Broadcaster {
 
 func (b *Broadcaster) BroadcastLog() {
 	for log := range b.tradeLogChan {
-		b.mu.Lock()
-		cons := b.clients[combine(log.EventHash, log.Maker)]
-		for _, c := range cons {
-			if err := c.ws.WriteJSON(log); err != nil {
-				b.l.Errorw("error when send msg", "err", err)
-			}
-		}
-		b.mu.Unlock()
+		b.writeEvent(log)
 	}
 }
 
-func (b *Broadcaster) addConn(event, maker string, conn *websocket.Conn) {
+func (b *Broadcaster) newConn(event, maker string, conn *websocket.Conn) {
 	id := xid.New().String()
 	b.l.Infow("connected socket", "id", id)
 	go func() {
 		msgType, msg, err := conn.ReadMessage()
 		b.l.Infow("read msg result", "id", id, "msgType", msgType, "msg", msg, "err", err)
 		if err != nil {
-			b.mu.Lock()
-			e, ok := b.clients[fmt.Sprintf("%s-%s", event, maker)]
-			if !ok {
-				return
-			}
-			conn.Close()
-			delete(e, id)
-			b.mu.Unlock()
+			b.removeConn(conn, id, event, maker)
 		}
 	}()
-	b.mu.Lock()
-	cons, ok := b.clients[combine(event, maker)]
-	if !ok {
-		cons = map[string]Con{}
-	}
-	cons[id] = Con{
-		id:        id,
-		ws:        conn,
-		maker:     maker,
-		eventHash: event,
-	}
-	b.clients[combine(event, maker)] = cons
-	b.mu.Unlock()
+	b.addConn(conn, id, event, maker)
 }
 
 func combine(event, maker string) string {
@@ -87,6 +61,44 @@ func (b *Broadcaster) Test() {
 		b.tradeLogChan <- storage.TradeLog{
 			EventHash: "0xac75f773e3a92f1a02b12134d65e1f47f8a14eabe4eaf1e24624918e6a8b269f",
 			Maker:     "0x807cF9A772d5a3f9CeFBc1192e939D62f0D9bD38",
+		}
+	}
+}
+
+func (b *Broadcaster) removeConn(conn *websocket.Conn, id, event, maker string) {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+	e, ok := b.clients[fmt.Sprintf("%s-%s", event, maker)]
+	if !ok {
+		return
+	}
+	conn.Close()
+	delete(e, id)
+}
+
+func (b *Broadcaster) addConn(conn *websocket.Conn, id, event, maker string) {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+	cons, ok := b.clients[combine(event, maker)]
+	if !ok {
+		cons = map[string]Con{}
+	}
+	cons[id] = Con{
+		id:        id,
+		ws:        conn,
+		maker:     maker,
+		eventHash: event,
+	}
+	b.clients[combine(event, maker)] = cons
+}
+
+func (b *Broadcaster) writeEvent(log storage.TradeLog) {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+	cons := b.clients[combine(log.EventHash, log.Maker)]
+	for _, c := range cons {
+		if err := c.ws.WriteJSON(log); err != nil {
+			b.l.Errorw("error when send msg", "err", err)
 		}
 	}
 }
