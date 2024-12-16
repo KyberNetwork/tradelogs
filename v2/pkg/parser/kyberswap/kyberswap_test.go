4a327bc14ddf38a3a606654fd4703b279abcaca4
Nguyen Thuy Linh <linh.nguyen@kyber.network>
2024-12-16 10:39:07 +0700

    add unit test

--- v2/pkg/parser/kyberswap/kyberswap_test.go
+++ v2/pkg/parser/kyberswap/kyberswap_test.go
@@ -37,6 +37,30 @@ func TestFetchEvent(t *testing.T) {
 }
 
 func TestParseEvent(t *testing.T) {
+	eventRaw := `{
+    "address": "0x617dee16b86534a5d792a4d7a62fb491b544111e",
+    "topics": [
+        "0xd6d4f5681c246c9f42c203e287975af1601f8df8035a9251f79aab5c8f09e2f8"
+    ],
+    "data": "0x000000000000000000000000db306e5c24cd28a02b50c6f893d46a3572835195000000000000000000000000eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee000000000000000000000000952193fcf398550b6caa0ecf5e706a6ad0ab8d8a000000000000000000000000db306e5c24cd28a02b50c6f893d46a357283519500000000000000000000000000000000000000000000000006f05b59d3b2000000000000000000000000000000000000000000000000000000005a91a0920233",
+    "blockNumber": "0xf87271",
+    "transactionHash": "0x6461e9058f2779725aa97bf5a80d0170967d36c5eddc387396359bb0caafe89c",
+    "transactionIndex": "0x1",
+    "blockHash": "0x8e4418b9ed9de011aaf66f8122488e6bc209f19b2a52653cd9e06780ac2f11c2",
+    "logIndex": "0xa",
+    "removed": false
+}`
+	event := types.Log{}
+	err := json.Unmarshal([]byte(eventRaw), &event)
+	require.NoError(t, err)
+	p := MustNewParser()
+	log, err := p.Parse(event, uint64(time.Now().Unix()))
+	require.NoError(t, err)
+	require.Equal(t, log[0].EventHash, p.eventHash)
+	t.Log(log)
+}
+
+func TestParseWithCallFrameEvent(t *testing.T) {
 	eventRaw := `{"address":"0x6131b5fae19ea4f9d964eac0408e4408b66337b5","topics":["0xd6d4f5681c246c9f42c203e287975af1601f8df8035a9251f79aab5c8f09e2f8"],"data":"0x0000000000000000000000002593154f192f46f65a4a0315a5d77556d5d3d264000000000000000000000000a0b86991c6218b36c1d19d4a2e9eb0ce3606eb48000000000000000000000000c02aaa39b223fe8d0a0e5c4f27ead9083c756cc20000000000000000000000002593154f192f46f65a4a0315a5d77556d5d3d264ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff0000000000000000000000000000000000000000000000000c904ae3a9b13e84","blockNumber":"0x14546f0","transactionHash":"0xb84d5ba76703a12ff65e7d4cd83a5e6ff533561162c922ad02695b6213d4b1c7","transactionIndex":"0x1","blockHash":"0x66806a6d24c19269a36f9b7a4a6738afc1d6a58d45811703bca1fce9e542e0be","logIndex":"0x91","removed":false}`
 	event := types.Log{}
 	err := json.Unmarshal([]byte(eventRaw), &event)
